1875c1ace8ab0a752517946f60ba46ee3cff9a05
Courtney Burns <courtneylynnburns@gmail.com>
2016-01-17 23:59:18 +0000

    fin

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,2 +1,9 @@
 def anagrams?(str1, str2)
+    if str1.length != str2.length
+        false
+    else
+        arr1=str1.split('')
+        arr2=str2.split('')
+    end
+    arr1.sort == arr2.sort
 end
